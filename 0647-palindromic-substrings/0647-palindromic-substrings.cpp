daa2fc954948e06a4e4fc4008878e94cf8b0a953
Vinayak Bisht <116460737+vinayakbisht@users.noreply.github.com>
2026-08-07 19:45:43 +0530

    Time: 4 ms (68.42%), Space: 8.5 MB (91.51%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
+    int count = 0;
+
+    void expand(string &s, int i, int j){
+        while(i>=0 && j<s.size() && s[i] == s[j]){
+            count++;
+            i--;
+            j++;
+        }
+    } 
     int countSubstrings(string s){
         int n = s.size();
 
-        vector<vector<bool>> t(n, vector<bool>(n,false));
-        int count = 0;
+        for(int i=0; i<n; i++){
 
-        for(int len=1; len<=n; len++){
-            for(int i=0; i+len-1<n; i++){
-                
-               int j = i+len-1;
-
-               if(i==j){                  //1 length
-                  t[i][j] = true;
-                }
-                else if(i+1 == j){      // 2 length
-                   t[i][j] = (s[i] == s[j]);
-
-                } else{
-                    t[i][j] = (s[i] == s[j] && t[i+1][j-1] == true);
-                }
-
-                if(t[i][j] == true) count++;
-            }
+            expand(s,i,i);
+            expand(s,i,i+1); 
         }
+        
         return count;
     }
 };
