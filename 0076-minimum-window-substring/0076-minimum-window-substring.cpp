771d968849355d6073d22195cf0d179ac172b94a
Vinayak Bisht <116460737+vinayakbisht@users.noreply.github.com>
2026-08-14 15:13:30 +0530

    Time: 7 ms (61.11%), Space: 11.3 MB (83.31%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> mpp;
-        int minLen = INT_MAX;
-        int countRequired = t.size();
+        if(t.size() > s.size()) return "";
 
-        for(int i=0; i<t.size(); i++){
+        unordered_map<char, int> mpp;
+        for(int i=0; i<t.size(); i++){    // store freq. of t's char.
             mpp[t[i]]++;
         }
 
-        int i=0;
-        int startI = -1;
+        int minLen = INT_MAX;
+        int countRequired = t.size();
+        int i=0; int start_i = -1;
 
         for(int j=0; j<s.size(); j++){
-           
-            if(mpp[s[j]] > 0){
-                countRequired--;
-            }
+            if(mpp[s[j]] > 0) countRequired--;
+            
             mpp[s[j]]--;
-
-            while(countRequired == 0){
+            while(countRequired == 0){    // start shrinking
                 if(j-i+1 < minLen){
                     minLen = j-i+1;
-                    startI = i;
+                    start_i = i;
                 }
                 if(++mpp[s[i]] > 0) countRequired++;
                 i++;
             }
-
         }
-
-        if(startI == -1){
-            return "";
-        }
+        if(start_i == -1) return "";
         
-        return s.substr(startI, minLen);
+        return s.substr(start_i, minLen);
     }
 };  
